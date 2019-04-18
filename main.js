14c9cdfb2a69148d1d9f3800372892fd469efdf9
more-ka <1640627628@qq.com>
2019-04-18 14:00:35 +0800

    使用函数优化

--- main.js
+++ main.js
@@ -1,42 +1,52 @@
-let $buttons = $('#buttons > span')
+let $buttons = $('#buttons>span')
 let $slides = $('#slides')
-let $images = $slides.children('img')
-let $firstcopy = $images.eq(0).clone(true)
-let $lastcopy = $images.eq(2).clone(true)
+let $images = $('#slides>img')
+let current = 0
 
-$slides.append($firstcopy)
-$slides.prepend($lastcopy)
-let current = 5
+makeFakeSlides()
 $slides.css({transform:'translateX(-250px)'})
-$buttons.eq(0).on('click',function(){
-  if(current === 2){
-    $slides.css({transform:'translateX(-1000px)'})
-    .one('transitionend',function(){
-      $slides.hide()
+bindEvent()
+
+function makeFakeSlides(){
+  let $firstCopy = $images.eq(0).clone(true)
+  let $lastCopy = $images.eq(2).clone(true)
+  $slides.append($firstCopy)
+  $slides.prepend($lastCopy)
+}
+
+function bindEvent(){
+  $buttons.eq(0).on('click',function(){
+    if(current === 2){
+      $slides.css({transform:'translateX(-1000px)'})
+      .one('transitionend',function(){
+        $slides.addClass('hide')
       $slides.css({transform:'translateX(-250px)'})
       .offset()
-      $slides.show()
-    })
-  }else{
-    $slides.css({transform:'translateX(-250px)'})
-    current = 0
-  }
-})
-$buttons.eq(1).on('click',function(){
-  $slides.css({transform:'translateX(-500px)'})
-  current = 1
-})
-$buttons.eq(2).on('click',function(){
-  if(current === 0){
-    $slides.css({transform:'translateX(0px)'})
-    .one('transitionend',function(){
-      $slides.hide()
+      $slides.removeClass('hide')
+      current = 0
+      })
+    }else{
+      $slides.css({transform:'translateX(-250px)'})
+      current = 0
+    }
+  })
+  $buttons.eq(1).on('click',function(){
+    $slides.css({transform:'translateX(-500px)'})
+    current = 1
+  })
+  $buttons.eq(2).on('click',function(){
+    if(current === 0){
+      $slides.css({transform:'translateX(0px)'})
+      .one('transitionend',function(){
+        $slides.addClass('hide')
+        $slides.css({transform:'translateX(-750px)'})
+        .offset()
+        $slides.removeClass('hide')
+        current = 2
+      })
+    }else{
       $slides.css({transform:'translateX(-750px)'})
-    .offset()
-    $slides.show()
-    })
-  }else{
-    $slides.css({transform:'translateX(-750px)'})
-    current = 2
-  }
-})
+      current = 2
+    }
+  })
+}
